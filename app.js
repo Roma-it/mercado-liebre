0d97955ee417a59000b79256134ada0c62d442c0
Rodrigo Roma <rmarsan@estudio-marsan.com.ar>
2021-01-29 20:24:07 -0300

    Proyecto Mercado Liebre

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname,"./public")))
+app.listen(4050, () => console.log('Servidor levantado en el puerto 4050'));
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")));
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")));
+
