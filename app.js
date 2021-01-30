24618461662baa7beda7b788130216ae7e12f72c
Rodrigo Roma <rmarsan@estudio-marsan.com.ar>
2021-01-29 21:01:38 -0300

    Configure el puerto y agregue el Proclife

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.resolve(__dirname,"./public")))
-app.listen(4050, () => console.log('Servidor levantado en el puerto 4050'));
+app.listen(process.env.PORT || 4050, () => console.log('Servidor levantado en el puerto 4050'));
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")));
 app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")));
 app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")));
